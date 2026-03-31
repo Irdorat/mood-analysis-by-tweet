878dc28f3f8c4f13a558ad7315e149f1b14ffe65
Irdorat <klykov.pasha@gmail.com>
2026-03-31 17:52:10 +0800

    Проведена настройка гиперпараметров модели Catboost

--- notebooks/catboost_tuningv2_model.py
+++ notebooks/catboost_tuningv2_model.py
@@ -12,17 +12,25 @@ def _():
     from catboost import CatBoostClassifier, Pool
     from sklearn.model_selection import train_test_split
     from sklearn.feature_extraction.text import TfidfVectorizer
-    from sklearn.metrics import accuracy_score, f1_score
+    from sklearn.metrics import accuracy_score, f1_score, roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score
     from scipy.sparse import hstack
     import matplotlib.pyplot as plt
     import seaborn as sns
+    import re
 
     return (
         CatBoostClassifier,
         Pool,
         accuracy_score,
+        auc,
+        confusion_matrix,
         f1_score,
+        mo,
         pd,
+        plt,
+        precision_score,
+        recall_score,
+        roc_curve,
         train_test_split,
     )
 
@@ -35,13 +43,9 @@ def _(pd):
     y=data['y']
     X['text_len'] = X['text'].str.len()
     X['word_count'] = X['text'].str.split().str.len()
-    X['has_exclamation'] = X['text'].str.contains('!').astype(int)
-    X['has_question'] = X['text'].str.contains('\?').astype(int)
-    X['upper_ratio'] = X['text'].str.count(r'[A-Z]') / (X['text'].str.len() + 1)
     X['unique_word_ratio'] = (
         X['text'].str.split().apply(lambda x: len(set(x)) / (len(x) + 1))
     )
-
     return X, y
 
 
@@ -99,12 +103,88 @@ def _(CatBoostClassifier, Pool, X, train_test_split, y):
 def _(X_test, X_train, accuracy_score, f1_score, model, pd, y_test, y_train):
     y_train_prediction_cb=model.predict(X_train)
     y_test_prediction_cb=model.predict(X_test)
-    pd.DataFrame(
-        {"Какая выборка?": ["Выборка train","Выборка test"],
+    df=pd.DataFrame(
+        {"Какая выборка?": ["Train","Test"],
          "Accuracy метрика": [accuracy_score(y_train, y_train_prediction_cb),accuracy_score(y_test,y_test_prediction_cb)],
          "F1-мера": [f1_score(y_train, y_train_prediction_cb),f1_score(y_test,y_test_prediction_cb)]
             }
     )
+    print(df)
+    return y_test_prediction_cb, y_train_prediction_cb
+
+
+@app.cell
+def _(auc, plt, roc_curve, y_test, y_test_prediction_cb):
+    #ROC-AUC
+    fpr_cb, tpr_cb, _ = roc_curve(y_test, y_test_prediction_cb) #false positive rate (доля ложноположительных срабатываний), true positive rate (чувствительность recall)
+    roc_auc_cb = auc(fpr_cb, tpr_cb) #AUC = 1 — идеальная классификация, 0.5 — случайная
+
+    plt.plot(fpr_cb, tpr_cb, label=f'Catboost Finetune (AUC = {roc_auc_cb:.3f})')
+
+    plt.plot([0,1],[0,1],'--') ## Эта линия соответствует случайной модели (AUC=0.5)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.legend()
+    plt.savefig('image/tune_catboost_roc_curve.png')
+    plt.show()
+    return
+
+
+@app.cell
+def _(
+    confusion_matrix,
+    pd,
+    precision_score,
+    recall_score,
+    y_test,
+    y_test_prediction_cb,
+    y_train,
+    y_train_prediction_cb,
+):
+    cm = confusion_matrix(y_test, y_test_prediction_cb) #матрица неточностей, показывающая TP, FN, TN, FP
+    cm_df = pd.DataFrame(
+        cm,
+        index=['Negative', 'Positive'],
+        columns=['Predicted Negative', 'Predicted Positive']
+    )
+
+    print(cm_df)
+    print(f"Precission for train: {precision_score(y_train, y_train_prediction_cb):.3f}\nRecall for train: {recall_score(y_train, y_train_prediction_cb):.3f}\nPrecission for test: {precision_score(y_test, y_test_prediction_cb):.3f}\nRecall for test: {recall_score(y_test, y_test_prediction_cb):.3f}")
+    return
+
+
+@app.cell
+def _(model):
+    #catboost напрямую не дает посмотреть важность слов, но дает оценить важность стобцов
+    fi = model.get_feature_importance(prettified=True)
+    print(fi.head(10))
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #Итог
+    Провели настройку гиперпараметров модели и получили прост в 2% качества модели
+
+    ##Результаты
+    |Метрика|Train|Test|
+    |:-----:|:----:|:--:|
+    |Accuracy|0.843899|0.823605|
+    |F1-мера|0.843448|0.823060|
+    |Recall|0.841|0.821|
+    |Precission|0.846|0.825|
+
+    ##Матрица неточностей
+    ||Прогноз y>0|Прогноз y<0|
+    |:-----:|:----:|:--:|
+    |Реал y>0|130992|28628|
+    |Реал y<0|27693|131976|
+
+    , где TP = 130992, TN = 131976, FP = 27693, FN = 28628
+
+    ##Следующий шаг - повысить качество модели используя ансамбль
+    """)
     return
 
 
